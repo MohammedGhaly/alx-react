01228d1ca31304ca49f4705ddacb0fcdd9155cdb
MohammedGhaly <medooghaly7@gmail.com>
2024-01-02 13:44:38 +0200

    redoing task 2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,13 +2,17 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: path.resolve(__dirname, "./js/dashboard_main.js"),
-  performance: {
-    maxAssetSize: 1000000,
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
   },
   output: {
-    filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
   module: {
     rules: [
@@ -17,7 +21,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(gif|png|jp?g|svg)$/i,
         use: [
           "file-loader",
           {
